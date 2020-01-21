074334acb03c30338e0fd884e7d9b9973d86ea5d
Rodrigo Neumann <rneumann@Rodrigos-MacBook-Pro.local>
2020-01-21 18:28:50 +0000

    added edit_propert, add_property layouts and form

--- seMgmtApp/routes.py
+++ seMgmtApp/routes.py
@@ -4,3 +4,19 @@ from seMgmtApp import app
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/add_property")
+def add_property():
+    return render_template("add_property.html")
+
+@app.route("/edit_property")
+def edit_property():
+    return render_template("edit_property.html")
