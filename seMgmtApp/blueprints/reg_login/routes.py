1d3ea39c0b4b8a844acb3b8b9cf27561970d17ef
Rodrigo Neumann <rodrigoneumann@gmail.com>
2020-02-16 18:44:15 +0000

    login page, routes and auth db working

--- seMgmtApp/blueprints/reg_login/routes.py
+++ seMgmtApp/blueprints/reg_login/routes.py
@@ -8,7 +8,7 @@ reg_login = Blueprint("reg_login", __name__)
 @reg_login.route("/register", methods=['POST', 'GET'])
 def register():
 
-# User registration - check user/password
+    # User registration - check user/password
     if request.method == 'POST':
         users = users_collection
         is_user = users.find_one(
@@ -19,15 +19,33 @@ def register():
             session['username'] = request.form['username'].lower()
             return redirect(url_for('main.index'))
         else:
-            flash("Sorry, '{}' is already in use, please choose another username.".format(request.form['username'].upper()))
+            flash("Sorry, '{}' is already in use, please choose another username.".format(
+                request.form['username'].upper()))
         return redirect(url_for('reg_login.register'))
-        
+
     return render_template('register.html')
 
 
-@reg_login.route("/login")
+@reg_login.route("/login", methods=['POST', 'GET'])
 def login():
-    return render_template('login.html', title="Login")
+
+    if request.method == 'POST':
+        users = users_collection
+        is_user = users.find_one(
+        {"username": request.form['username'].lower()})
+
+        if is_user:
+            if request.form['password'] == is_user['password']:
+                session['username'] = request.form['username']
+                return redirect(url_for('main.index'))
+            else:
+                flash("Incorrect username or password. Please try again.")
+                return redirect(url_for('reg_login.login'))
+        else:
+            flash("Username {} is not yet registered.".format(
+                request.form['username'].upper()))
+    return render_template('login.html')
+
 
 @reg_login.route("/logout")
 def logout():
