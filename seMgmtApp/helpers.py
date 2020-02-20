79524cef15bacb916a8ec020e02d63a77fe305fb
Rodrigo Neumann <rodrigoneumann@gmail.com>
2020-02-20 10:54:23 +0000

    edited helpers for add property dropdown, created local lists instead server db, some layout issues fixed in property details page

--- seMgmtApp/helpers.py
+++ seMgmtApp/helpers.py
@@ -7,20 +7,20 @@ users_collection = mongo.db.users
 
 # Properties Type Dropdown List
 def dropdown_properties_type():
-    property_type_list = properties_collection.distinct("property_type")
+    property_type_list = ['Apartment', 'House', 'Other']
     return property_type_list
 
 # Num beds Dropdown List
 def dropdown_num_beds():
-    num_bed_list = properties_collection.distinct("num_bed")
+    num_bed_list = ['1 bedroom', '2 bedrooms', '3 bedrooms', '4 bedrooms', '5 bedrooms']
     return num_bed_list
 
 # Num baths Dropdown List
 def dropdown_num_baths():
-    num_bath_list = properties_collection.distinct("num_bath")
+    num_bath_list = ['1 bathroom', '2 bathrooms', '3 bathrooms', '4 bathrooms']
     return num_bath_list
 
 # Districts Dropdown List
 def dropdown_districts():
-    district_list = properties_collection.distinct("district")
+    district_list = ['Eastleigh', 'Fareham', 'Gosport', 'Havant', 'Portsmouth', 'Other']
     return district_list
