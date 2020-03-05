9a21f8d58d670edfef26e3f2ec967afe778d33b6
Rodrigo Neumann <rodrigoneumann@gmail.com>
2020-03-05 16:12:35 +0000

    property routes auth checking issues fixed

--- semgmtapp/blueprints/properties/routes.py
+++ semgmtapp/blueprints/properties/routes.py
@@ -17,6 +17,7 @@ properties = Blueprint("properties", __name__)
 @properties.route("/add_property", methods=["GET", "POST"])
 def add_property():
 
+    # Check if there is a session
     if session.get("username"):
         if request.method == "GET":
 
@@ -43,56 +44,90 @@ def add_property():
             # Flash Alert with confirmation
             flash("Your Ad has been successfully added.")
             return redirect(url_for("properties.my_ads"))
-    else:
-        flash("You must be logged in to view this page.")
-        return redirect(url_for("reg_login.login"))
+
+    flash("You must be logged in to view this page.")
+    return redirect(url_for("reg_login.login"))
 
 
 @properties.route("/edit_property/<property_id>", methods=["GET", "POST"])
 def edit_property(property_id):
 
-    if request.method == "GET":
-        type_list = dropdown_properties_type()
-        num_beds_list = dropdown_num_beds()
-        num_baths_list = dropdown_num_baths()
-        districts_list = dropdown_districts()
-
-        property_details = properties_collection.find({"_id": ObjectId(property_id)})
+    # Check if there is a session
+    if session:
+        # Check if session user is the ads owner
+        property = properties_collection.find_one(
+            ObjectId(property_id), {"agent": 1, "_id": 0}
+        )
         agent = session["username"].upper()
+        if agent == property["agent"]:
+            # Request the property information to the base
+            if request.method == "GET":
+                type_list = dropdown_properties_type()
+                num_beds_list = dropdown_num_beds()
+                num_baths_list = dropdown_num_baths()
+                districts_list = dropdown_districts()
 
-        return render_template(
-            "edit_property.html",
-            property_details=property_details,
-            agent=agent,
-            types=type_list,
-            num_bed=num_beds_list,
-            num_bath=num_baths_list,
-            districts=districts_list,
-        )
+                property_details = properties_collection.find(
+                    {"_id": ObjectId(property_id)}
+                )
+                agent = session["username"].upper()
 
-    if request.method == "POST":
+                return render_template(
+                    "edit_property.html",
+                    property_details=property_details,
+                    agent=agent,
+                    types=type_list,
+                    num_bed=num_beds_list,
+                    num_bath=num_baths_list,
+                    districts=districts_list,
+                )
+            # Send the updated info to the db
+            if request.method == "POST":
 
-        properties_collection.update_one(
-            {"_id": ObjectId(property_id)}, {"$set": request.form.to_dict()}
-        )
-        # Flash Alert with update confirmation
-        flash("The property details have been updated successfully.")
-        return redirect(url_for("properties.property_details", property_id=property_id))
+                properties_collection.update_one(
+                    {"_id": ObjectId(property_id)}, {"$set": request.form.to_dict()}
+                )
+                # Flash Alert with update confirmation
+                flash("The property details have been updated successfully.")
+                return redirect(
+                    url_for("properties.property_details", property_id=property_id)
+                )
+        # If not owner redirect to my_ads page
+        flash("You must be the profile owner to edit that property")
+        return redirect(url_for("properties.my_ads"))
+    # If not logged in, redirect to login page
+    flash("You must be logged in to view this page.")
+    return redirect(url_for("reg_login.login"))
 
 
 @properties.route("/delete_property/<property_id>", methods=["GET", "POST"])
 def delete_property(property_id):
 
-    properties_collection.delete_one({"_id": ObjectId(property_id)})
-    # Flash Alert with update confirmation
-    flash("The property has been successfully deleted from your list.")
-
-    return redirect(url_for("properties.my_ads"))
+    # Check if there is a session
+    if session:
+        # Check if session user is the ads owner
+        property = properties_collection.find_one(
+            ObjectId(property_id), {"agent": 1, "_id": 0}
+        )
+        agent = session["username"].upper()
+        if agent == property["agent"]:
+            properties_collection.delete_one({"_id": ObjectId(property_id)})
+            # Flash Alert with delete confirmation
+            flash("The property has been successfully deleted from your list.")
+            return redirect(url_for("properties.my_ads"))
+        # If not owner redirect to my_ads page
+        flash("You must be the profile owner to delete that property")
+        return redirect(url_for("properties.my_ads"))
+    # If not logged in, redirect to login page
+    flash("You must be logged in to view this page.")
+    return redirect(url_for("reg_login.login"))
 
 
 @properties.route("/my_ads", methods=["GET", "POST"])
 def my_ads():
 
+    # Only the ads of the logged-in agent will be displayed.
+    # All ads on MY_ADS page will have the EDIT and DELETE buttons enabled
     if session.get("username"):
         # Get the session username and set for the agent variable
         agent = session["username"].upper()
@@ -163,13 +198,14 @@ def my_ads():
             pages=num_pages,
         )
 
-    else:
-        flash("You must be logged in to view this page.")
-        return redirect(url_for("reg_login.login"))
+    flash("You must be logged in to view this page.")
+    return redirect(url_for("reg_login.login"))
 
 
 @properties.route("/properties_list", methods=["GET", "POST"])
 def list_properties():
+
+    # Authentication is not necessary, the list of ads will be displayed to all users.
 
     # value of selection , sale, rent or all in searching dropdown
     search = request.args.get("search")
@@ -229,7 +265,7 @@ def list_properties():
 @properties.route("/property_details/<property_id>", methods=["GET", "POST"])
 def property_details(property_id):
 
-    # checks if there is a user logged in, if yes, the variable agent for checking property is rendered
+    # checks if there is a user logged in, if yes, the variable agent for check property is rendered
     if session.get("username"):
         property_details = properties_collection.find({"_id": ObjectId(property_id)})
         agent = session["username"].upper()
@@ -237,6 +273,5 @@ def property_details(property_id):
             "property.html", property_details=property_details, agent=agent
         )
 
-    else:
-        property_details = properties_collection.find({"_id": ObjectId(property_id)})
-        return render_template("property.html", property_details=property_details)
+    property_details = properties_collection.find({"_id": ObjectId(property_id)})
+    return render_template("property.html", property_details=property_details)
